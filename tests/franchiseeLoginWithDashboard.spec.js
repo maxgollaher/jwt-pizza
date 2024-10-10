183430e4089cd1533be1c9b1607d99d759ed3dd2
Max Gollaher <max.c.gollaher@gmail.com>
2024-10-09 23:42:02 -0600

    fixing tests

--- tests/franchiseeLoginWithDashboard.spec.js
+++ tests/franchiseeLoginWithDashboard.spec.js
@@ -1,29 +1,17 @@
 import { test, expect } from 'playwright-test-coverage';
 
-test('admin login, dashboard, add franchise', async ({ page }) => {
-    const defaultAdmin = { name: '常用名字', email: 'a@jwt.com', password: 'admin' };
+test('franchisee login, dashboard, add store', async ({ page }) => {
+    const defaultUser = { name: 'pizza franchisee', email: 'f@jwt.com', password: 'franchisee' };
 
     await page.route('*/api/auth', async (route) => {
-        const loginReq = { email: defaultAdmin.email, password: defaultAdmin.password };
+        const loginReq = { email: defaultUser.email, password: defaultUser.password };
         const loginRes = {
-            user: { id: 1, name: defaultAdmin.name, email: defaultAdmin.email, roles: [{ role: 'admin' }] },
-            token: 'abcdef',
+            user: { id: 2, name: defaultUser.name, email: defaultUser.email, roles: [{ role: 'diner' }] },
+            token: 'token2',
         };
         expect(route.request().method()).toBe('PUT');
         expect(route.request().postDataJSON()).toMatchObject(loginReq);
         await route.fulfill({ json: loginRes });
-    });
-
-    await page.route('*/api/franchise', async (route) => {
-        const createFranchiseReq = { name: 'pizzaPocket', admins: [{ email: 'f@jwt.com' }] };
-        const createFranchiseRes = {
-            name: 'pizzaPocket',
-            admins: [{ email: 'f@jwt.com', id: 4, name: 'pizza franchisee' }],
-            id: 1,
-        };
-        expect(route.request().method()).toBe('POST');
-        expect(route.request().postDataJSON()).toMatchObject(createFranchiseReq);
-        await route.fulfill({ json: createFranchiseRes });
     });
 
     await page.route('*/api/franchise/1/store', async (route) => {
@@ -48,23 +36,35 @@ test('admin login, dashboard, add franchise', async ({ page }) => {
         }
     });
 
-    // Test flow: login -> dashboard -> add franchise -> logout
+    // Test flow: login -> dashboard -> add store -> logout
     await page.goto('/');
     await page.getByRole('link', { name: 'Login' }).click();
 
     // Perform login
-    await page.getByPlaceholder('Email address').fill(defaultAdmin.email);
-    await page.getByPlaceholder('Password').fill(defaultAdmin.password);
+    await page.getByPlaceholder('Email address').fill(defaultUser.email);
+    await page.getByPlaceholder('Password').fill(defaultUser.password);
     await page.getByRole('button', { name: 'Login' }).click();
 
-    await expect(page.locator('#navbar-dark')).toContainText('Admin');
+    // Go to franchise dashboard
+    await page.goto('/franchise-dashboard');
+
+    // Add store
+    await expect(page.getByRole('button', { name: 'Create store' })).toBeVisible();
+    await page.getByRole('button', { name: 'Create store' }).click();
+    await page.getByPlaceholder('store name').fill('test store');
+    await page.getByRole('button', { name: 'Create' }).click();
+
+    // Verify store addition
+    await expect(page.locator('tbody')).toContainText('test store');
+
+    // Delete Store
+    await page.getByRole('button', { name: 'Close' }).click();
+    await page.getByRole('button', { name: 'Close' }).click();
     await page.getByRole('link', { name: 'Admin' }).click();
 
-    // Add franchise
-    await page.getByRole('button', { name: 'Add Franchise' }).click();
-    await page.getByPlaceholder('franchise name').fill('pizzaPocket');
-    await page.getByPlaceholder('franchisee admin email').fill('f@jwt.com');
-    await page.getByRole('button', { name: 'Create' }).click();
+    // Close Franchise
+    await page.getByRole('button', { name: 'Close' }).click();
+    await page.getByRole('button', { name: 'Close' }).click();
 
     // Logout
     await page.getByRole('link', { name: 'Logout' }).click();
